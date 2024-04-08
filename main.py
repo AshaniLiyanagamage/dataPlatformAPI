db68a4132a2996ed640451c15cf33c62bf9ffda6
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-04-08 13:30:49 +0530

    updated

--- main.py
+++ main.py
@@ -15,12 +15,7 @@ html = f"""
     </head>
     <body>
         <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
-            <h1>Hello from FastAPI@{__version__}</h1>
-            <ul>
-                <li><a href="/docs">/docs</a></li>
-                <li><a href="/redoc">/redoc</a></li>
-            </ul>
-            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+            hello world
         </div>
     </body>
 </html>
